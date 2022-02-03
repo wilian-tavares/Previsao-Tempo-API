f70a4be7a56d4b0bfbbf28f69012bc67452b03b0
wilian-tavares <wilian.tavares@souunit.com.br>
2022-02-03 18:05:53 -0300

    finalização do projeto

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
 // const chave = '8bcc6a8c69b996b7302141521d37b84a';
-// const pais = 55;
-// const cep = 49030650;
-// const cidade = 'Aracaju'
-
-//const city = prompt('Digite a cidade')
 
 const city = document.getElementById('form-cidade');
-city.addEventListener('onmouseover',)
 
+const atual = document.getElementById('atual');
+const maxima = document.getElementById('maxima');
+const minima = document.getElementById('minima');
+const cidadeAtual = document.getElementById('cidadeAtual');
 
-// const url = 'https://api.openweathermap.org/data/2.5/weather?q=Aracaju&units=metric&appid=3d6a3d1fc18e2c50061da8437fb6b0b9'
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3d6a3d1fc18e2c50061da8437fb6b0b9`
-
-
-async function dbd() {
+async function tempo(city) {
     try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3d6a3d1fc18e2c50061da8437fb6b0b9`;
+
         const data = await fetch(url)
         const json = await data.json();
-
-
-        console.log(json);
-        console.log(json.name)
 
         const cidade = json.cidade;
         const pais = json.sys.country;
 
         const temperaturaAtual = json.main.temp;
-        const maxima = json.main.temp_max;
-        const minima = json.main.temp_min;
+        const temperaturaMaxima = json.main.temp_max;
+        const temperaturaMinima = json.main.temp_min;
 
+        atual.textContent = `Atual: ${temperaturaAtual.toFixed(1)}°`;
+        minima.textContent = `Mínima: ${temperaturaMinima.toFixed(1)}°`;
+        maxima.textContent = `Máxima: ${temperaturaMaxima.toFixed(1)}°`;
+        cidadeAtual.textContent = `${cidades} - ${pais}`
 
-        console.log(`Atual: ${temperaturaAtual} 
-                        \nMinima: ${minima}° 
-                        \nMáxima: ${maxima}°`)
-        console.log(pais)
-
+        console.log(`${cidades} - ${pais} 
+                        \nAtual: ${temperaturaAtual.toFixed(1)}° 
+                        \nMinima: ${minima.toFixed(1)}° 
+                        \nMáxima: ${maxima.toFixed(1)}°`)
 
     } catch (error) {
         console.log(error);
     }
 }
-dbd()
+
+
+city.addEventListener('keyup', () => {
+    const cidades = city.value;
+    tempo(cidades)
+})
+
 
